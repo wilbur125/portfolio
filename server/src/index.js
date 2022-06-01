0bc7e0a9f06d71a1f8041723ef1cf9bb324342e5
wilbur125 <65524382+wilbur125@users.noreply.github.com>
2022-06-01 15:00:34 -0700

    Added middleware

--- server/src/index.js
+++ server/src/index.js
@@ -3,6 +3,10 @@ const app = express();
 
 const PORT  = process.env.PORT || 9000;
 
+
+//Middleware
+app.use(express.static('client'));
+
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
