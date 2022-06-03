be4dacc4ceef6f69a7102700242911b8eb83c3c1
wilbur125 <65524382+wilbur125@users.noreply.github.com>
2022-06-03 14:17:15 -0700

    Coded SMTP to send contact forms to email

--- client/js/app.js
+++ client/js/app.js
@@ -3,34 +3,19 @@ const contactForm = document.querySelector('.contact-form');
 contactForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
-    let name = document.getElementById('name');
-    let email = document.getElementById('email');
-    let subject = document.getElementById('subject');
-    let message = document.getElementById('message');
-    
-    let formData = {
-        name: name.value,
-        email: email.value,
-        subject: subject.value,
-        message: message.value
-    };
-
-    let xhr = new XMLHttpRequest();
-    xhr.open('POST', '/');
-    xhr.setRequestHeader('content-type', 'application/json');
-    xhr.onload = function(){
-        console.log(xhr.responseText);
-
-        if (xhr.responseText == 'success'){
-            alert('Email sent');
-            name.value = '';
-            email.value = '';
-            subject.value = '';
-            message.value = '';
-        } else {
-            alert('Something went wrong!')
-        }
+    function sendEmail() {
+        Email.send({
+            SecureToken : "198e5610-0a47-4d5f-858b-fc5463f33fae",
+            To : 'matt@wilbur125.dev',
+            From : document.getElementById("email").value,
+            Subject : document.getElementById("subject").value,
+            Body : "Name: " + document.getElementById("name").value 
+                + "<br> Email: " + document.getElementById("email").value
+                + "<br> Message: " + document.getElementById("message").value
+        }).then(
+        message => alert(message)
+        );
     }
 
-    xhr.send(JSON.stringify(formData));
-});
+    sendEmail();
+})
