c1d42030d80afaedcddbc8789e1a1656a404ad98
wilbur125 <65524382+wilbur125@users.noreply.github.com>
2022-06-08 13:24:48 -0700

    Updated code to reset after sending the message

--- js/contactForm.js
+++ js/contactForm.js
@@ -1,19 +1,17 @@
 (function() {
-    // https://dashboard.emailjs.com/admin/account
     emailjs.init('YK7n8jA-r6lU9QaXy');
 })();
 
-window.onload = function() {
+window.onload = function () {
     document.getElementById('contact-form').addEventListener('submit', function(event) {
         event.preventDefault();
-        // generate a five digit number for the contact_number variable
         this.contact_number.value = Math.random() * 100000 | 0;
-        // these IDs from the previous steps
         emailjs.sendForm('service_g62huhk', 'template_1t5mpla', this)
             .then(function() {
                 console.log('SUCCESS!');
             }, function(error) {
                 console.log('FAILED...', error);
             });
+            document.getElementById('contact-form').reset();
     });
 };
