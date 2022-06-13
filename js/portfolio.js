c33068abaa26fbf3e2fd0c1fff2439db03f12da7
wilbur125 <65524382+wilbur125@users.noreply.github.com>
2022-06-13 10:53:56 -0700

    Updated project names and descriptions

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,10 +1,10 @@
 let projects = [
     {
-        name: "Project 1",
+        name: "TinyHouse App",
         image: "./assets/images/thlogo-white.jpg",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Et beatae enim sequi rem nostrum repellat, accusantium laborum dignissimos delectus a inventore quas libero explicabo suscipit officia repudiandae? Error, nam velit!",
-        url: "google.com",
-        github: "google.com"
+        description: "TinyHouse is a Full Stack React Masterclass offered by <a href='https://www.newline.co/tinyhouse'>newline</a> and utilizes React, Node, TypeScript, and GraphQL. It is a production ready application inspired by AirBnb. Users can be authenticated through Google OAuth, create listings, search for available listings around the world, schedule bookings, and send/receive payments via Stripe.",
+        url: "https://wilbur125-tinyhouse.herokuapp.com/",
+        github: "https://github.com/wilbur125/tinyhouse-dev"
     },
     {
         name: "Project 2",
