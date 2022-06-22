a34afc509c04fbfa4f5ba8fac35906c9df74c9ea
wilbur125 <65524382+wilbur125@users.noreply.github.com>
2022-06-22 09:55:55 -0700

    Modified list of projects

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,45 +1,35 @@
 let projects = [
     {
+        id: "tinyhouse-app",
         name: "TinyHouse App",
-        image: "./assets/images/thlogo-white.jpg",
-        description: "TinyHouse is a Full Stack React Masterclass offered by <a href='https://www.newline.co/tinyhouse'>newline</a> and utilizes React, Node, TypeScript, and GraphQL. It is a production ready application inspired by AirBnb. Users can be authenticated through Google OAuth, create listings, search for available listings around the world, schedule bookings, and send/receive payments via Stripe.",
+        image: "./assets/images/portfolio/temp.png",
+        cardDescription: "Here is some text",
         url: "https://wilbur125-tinyhouse.herokuapp.com/",
         github: "https://github.com/wilbur125/tinyhouse-dev"
     },
     {
-        name: "Entertainer Website",
-        image: "./assets/images/thlogo-white.jpg",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Et beatae enim sequi rem nostrum repellat, accusantium laborum dignissimos delectus a inventore quas libero explicabo suscipit officia repudiandae? Error, nam velit!",
-        url: "",
+        id: "project-2",
+        name: "Project #2",
+        image: "./assets/images/portfolio/temp.png",
+        cardDescription: "Here is some text",
+        url: "#",
         github: "./assets/images/thlogo-white.jpg"
     },
     {
-        name: "Restaurant Website",
-        image: "./assets/images/thlogo-white.jpg",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Et beatae enim sequi rem nostrum repellat, accusantium laborum dignissimos delectus a inventore quas libero explicabo suscipit officia repudiandae? Error, nam velit!",
-        url: "",
-        github: ""
-    },
-    {
-        name: "Vent",
-        image: "./assets/images/thlogo-white.jpg",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Et beatae enim sequi rem nostrum repellat, accusantium laborum dignissimos delectus a inventore quas libero explicabo suscipit officia repudiandae? Error, nam velit!",
-        url: "",
-        github: ""
-    },
-    {
+        id: "workout-tracker",
         name: "Workout Tracker",
-        image: "./assets/images/thlogo-white.jpg",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Et beatae enim sequi rem nostrum repellat, accusantium laborum dignissimos delectus a inventore quas libero explicabo suscipit officia repudiandae? Error, nam velit!",
-        url: "",
-        github: ""
+        image: "./assets/images/portfolio/temp.png",
+        cardDescription: "Here is some text",
+        url: "#",
+        github: "#"
     },
     {
-        name: "Project 6",
-        image: "./assets/images/thlogo-white.jpg",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Et beatae enim sequi rem nostrum repellat, accusantium laborum dignissimos delectus a inventore quas libero explicabo suscipit officia repudiandae? Error, nam velit!",
-        url: "",
-        github: ""
+        id: "code-quiz",
+        name: "Code Quiz",
+        image: "./assets/images/portfolio/temp.png",
+        cardDescription: "Here is some text",
+        url: "https://wilbur125.github.io/Code_Quiz/",
+        github: "https://github.com/wilbur125/Code_Quiz"
     }
 ];
 
@@ -48,23 +38,23 @@ const portfolio = document.querySelector("#portfolio-display");
 portfolio.innerHTML = projects.map(function(project) {
     return (
         `
-            <div class="col s12 m6 l4" >
+            <div class="col s12 m6 l3" >
                 <div class="card">
-                    <div class="card-image">
-                        <img src="${project.image}">
-                        <span class="card-title black-text">${project.name}</span>
-                    </div>
-                    <div class="card-content">
-                        <p>${project.description}</p>
-                    </div>
-                    <div class="card-action">
-                        <a href="${project.url}">${project.name} URL</a>
-                    </div>
-                    <div class="card-action">
-                        <a href="${project.github}">GitHub Repository</a>
+                    <a href="${project.id}.html">    
+                        <div class="card-image">
+                            <img class="responsive-img" src="${project.image}">
+                            <span class="card-title black-text">${project.name}</span>
+                        </div>
+                        <div class="card-content">
+                            <p>${project.cardDescription}</p>
+                        </div>
+                    </a>
+                    <div class="card-action center-align">
+                        <a class="pointer" href="${project.github}" target="_blank">GitHub Repository</a>
                     </div>
                 </div>
             </div>
         `
     )
 }).join('');
+
