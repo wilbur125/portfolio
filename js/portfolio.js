b77e6708581c14f5d05205304fa4bfde9b435d9d
wilbur125 <65524382+wilbur125@users.noreply.github.com>
2022-06-23 17:04:43 -0700

    Updated card description for TinyHouse App

--- js/portfolio.js
+++ js/portfolio.js
@@ -3,7 +3,7 @@ let projects = [
         id: "tinyhouse-app",
         name: "TinyHouse App",
         image: "./assets/images/portfolio/temp.png",
-        cardDescription: "Here is some text",
+        cardDescription: "TinyHouse is a Full Stack React Masterclass offered by newline and utilizes React, Node, TypeScript, and GraphQL. This project is inspired by the functionality of AirBnb.",
         url: "https://wilbur125-tinyhouse.herokuapp.com/",
         github: "https://github.com/wilbur125/tinyhouse-dev"
     },
